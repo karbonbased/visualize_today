26b5e81d31006cfa23f4c603d7ebef78305ed104
Joshua Davis <karbonbased@gmail.com>
2016-02-22 21:34:54 -0500

    building ajax calls in scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,32 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
 	console.log("working");
 
-
- $(function() {
-    $( "#accordion" ).accordion({
-      collapsible: true
+$(function(){
+    $.ajax({
+      url: 'http://localhost:27017/visualize',
+      type: 'get',
+      dataType: 'jsonp',
+      jsonp: 'jsonp', // mongod is expecting the parameter name to be called "jsonp"
+      success: function (data) {
+        console.log('success', data);
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+        console.log('error', errorThrown);
+      }
     });
   });
 
- });
+var sortedTasks = $('#image-box');
+
+for (var i = 0; i < tasks.length; i++) {
+	console.log(tasks[i].id);
+}
+
+ // $(function() {
+ //    $( "#accordion" ).accordion({
+ //      collapsible: true
+ //    });
+ //  });
+
+ // });
