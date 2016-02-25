15c1c3ef0df69905d5689068afd0a211f6efa830
Joshua Davis <karbonbased@gmail.com>
2016-02-25 09:26:56 -0500

    user icon field now required

--- models/users.js
+++ models/users.js
@@ -8,7 +8,7 @@ var userSchema = mongoose.Schema({
 	lastName: String,
 	email: String,
 	password: String,
-	userIcon: {type: String, default:"../img/usericons/usericon-master.jpg" },
+	userIcon: {type: String, required: true, default:"../img/usericons/usericon-master.jpg" },
 	dateCreated: { type:Date, default:Date.now },
 	tasks: [TaskSchema]
 })
